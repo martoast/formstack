bd76acedd420f166ddd6a3ccee1c7205e386b268
martoast <alexmartos96@gmail.com>
2023-11-02 12:12:21 -0700

    geofencing

--- app.vue
+++ app.vue
@@ -2,7 +2,17 @@
   <div>
     <iframe v-if="isWithinRadius" src="https://pinclick.formstack.com/forms/rosarito_test"></iframe>
     <div v-else>
-      <p>You must be within a 10km radius of Rosarito to access this page.</p>
+      <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div class="mx-auto max-w-2xl text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No estás en Rosarito.</h2>
+        <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">Tienes que estar dentro de un radio de 10 km de Rosarito para acceder a esta encuesta.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Realizar otra encuesta</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">ver más <span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+    </div>
+
     </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/*.{html,js,ts,vue}',
+    './layouts/*.{html,js,ts,vue}',
+    './pages/*.{html,js,ts,vue}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: '#F5A623',
+      secondary: '#ecc94b',
+      'white': '#ffffff',
+      'red': '#dc3545',
+      'gray-700': '#374151',
+      'gray-900': '#111827'
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
